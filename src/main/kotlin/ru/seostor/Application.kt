041d154dcfee7853335d2ee18c40a6a4597a53af
spiritofenrgy <spiritofenergy220@gmail.com>
2022-12-12 15:00:24 +0300

    add dependency

---
+++ src/main/kotlin/ru/seostor/Application.kt
@@ -0,0 +1,15 @@
+package ru.seostor
+
+import io.ktor.server.application.*
+import ru.seostor.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureMonitoring()
+    configureSerialization()
+    configureSecurity()
+    configureRouting()
+}
